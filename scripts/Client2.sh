b03f4b3af1fd389dc62d237d16304149e4f210d3
KobeVanolst2 <11601465@student.pxl.be>
2020-10-19 17:56:53 +0200

    Update Client2.sh

--- scripts/Client2.sh
+++ scripts/Client2.sh
@@ -1,1 +1,6 @@
+sudo systemctl start docker
+sudo systemctl start consul 
+sudo systemctl start nomad 
 
+consul agent -config-file /vagrant/scripts/ConsulClient2Config.hcl
+nomad agent -config /vagrant/scripts/NomadClient2Config.hcl
