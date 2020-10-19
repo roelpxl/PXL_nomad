8dd9061c0cd96c7c41c5fa30bf5a5300ac1ec7c2
KobeVanolst2 <11601465@student.pxl.be>
2020-10-19 17:53:01 +0200

    Update Server.sh

--- scripts/Server.sh
+++ scripts/Server.sh
@@ -2,6 +2,7 @@ sudo systemctl start docker
 sudo systemctl start consul 
 sudo systemctl start nomad 
 
+consul agent -config-file /vagrant/scripts/ConsulServerConfig
 nomad agent -config /vagrant/scripts/NomadServerConfig
 
 
