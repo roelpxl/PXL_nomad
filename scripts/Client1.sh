5eaa675c13e57d15dc2bc57a5ff316b6dde54bbe
roelpxl <56788051+roelpxl@users.noreply.github.com>
2020-10-25 01:58:21 +0200

    Update Client1.sh

--- scripts/Client1.sh
+++ scripts/Client1.sh
@@ -2,7 +2,7 @@ sudo systemctl start docker
 sudo systemctl start consul 
 sudo systemctl start nomad 
 
-sudo echo "192.168.1.4 puppet" | sudo tee -a /etc/hosts
+sudo echo "192.168.1.4 web" | sudo tee -a /etc/hosts
 
 consul agent -config-file /vagrant/scripts/ConsulClientConfig.hcl
 nomad agent -config /vagrant/scripts/NomadClientConfig.hcl &
