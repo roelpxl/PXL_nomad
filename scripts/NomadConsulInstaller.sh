4ea5dced75113d8d26fbc97dad95142c6e961a49
roelpxl <56788051+roelpxl@users.noreply.github.com>
2020-10-25 01:54:48 +0200

    Update NomadConsulInstaller.sh

--- scripts/NomadConsulInstaller.sh
+++ scripts/NomadConsulInstaller.sh
@@ -2,3 +2,5 @@ sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
 sudo yum -y install consul
 sudo yum -y install nomad
+sudo echo "192.168.1.5 node" | sudo tee -a /etc/hosts
+sudo echo "192.168.1.6 node2" | sudo tee -a /etc/hosts
