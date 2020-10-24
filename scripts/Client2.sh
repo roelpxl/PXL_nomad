56506f3250dca7149a27d301839230fc76a9fe4f
roelpxl <56788051+roelpxl@users.noreply.github.com>
2020-10-24 19:52:42 +0200

    Update Client2.sh

--- scripts/Client2.sh
+++ scripts/Client2.sh
@@ -3,4 +3,4 @@ sudo systemctl start consul
 sudo systemctl start nomad 
 
 consul agent -config-file /vagrant/scripts/ConsulClient2Config.hcl
-nomad agent -config /vagrant/scripts/NomadClient2Config.hcl
+nomad agent -config /vagrant/scripts/NomadClient2Config.hcl &
