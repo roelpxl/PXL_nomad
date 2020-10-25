27253d3bfa5a5b2b9798a4ad5604c49ab6975ba7
roelpxl <56788051+roelpxl@users.noreply.github.com>
2020-10-25 11:18:52 +0100

    Update NomadConsulInstaller.sh

--- scripts/NomadConsulInstaller.sh
+++ scripts/NomadConsulInstaller.sh
@@ -2,4 +2,10 @@ sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
 sudo yum -y install consul
 sudo yum -y install nomad
-sudo yum -y install docker
+
+echo "-------Try to install docker---------"
+
+sudo yum install -y yum-utils
+sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
