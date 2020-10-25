ce3e85d8e94d35a5283c7585e1407b1e5c25a81a
roelpxl <56788051+roelpxl@users.noreply.github.com>
2020-10-25 14:56:13 +0100

    Update NomadConsulInstaller.sh

--- scripts/NomadConsulInstaller.sh
+++ scripts/NomadConsulInstaller.sh
@@ -9,3 +9,6 @@ sudo yum install -y yum-utils
 sudo yum-config-manager \
     --add-repo \
     https://download.docker.com/linux/centos/docker-ce.repo
+    
+sudo yum -y install docker-ce docker-ce-cli containerd.io
+sudo yum -y install docker
