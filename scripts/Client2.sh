01a5c34ef220eefa8f7cdfa34f044cab6864b064
roelpxl <56788051+roelpxl@users.noreply.github.com>
2020-10-25 02:47:28 +0200

    Update Client2.sh

--- scripts/Client2.sh
+++ scripts/Client2.sh
@@ -3,6 +3,6 @@ sudo systemctl start consul
 sudo systemctl start nomad 
 
 sudo echo "192.168.1.4 server" | sudo tee -a /etc/hosts
-
-consul agent -config-file /vagrant/scripts/ConsulClient2Config.hcl
-nomad agent -config /vagrant/scripts/NomadClient2Config.hcl &
+echo "DEVICE=eth0" > /etc/sysconfig/network-scripts/ifcfg-eth0 && service network restart
+consul agent -config-file /vagrant/scripts/ConsulClient2Config.hcl /dev/null 2>&1 & 
+nomad agent -config /vagrant/scripts/NomadClient2Config.hcl /dev/null 2>&1 & 
