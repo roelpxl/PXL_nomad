dfd11d1e408cadc3e838d84f8c6083b3033eb769
roelpxl <56788051+roelpxl@users.noreply.github.com>
2020-10-25 02:53:27 +0200

    Update Server.sh

--- scripts/Server.sh
+++ scripts/Server.sh
@@ -4,7 +4,7 @@ sudo systemctl start nomad
 
 sudo echo "192.168.1.5 node" | sudo tee -a /etc/hosts
 sudo echo "192.168.1.6 node2" | sudo tee -a /etc/hosts
-echo "DEVICE=eth0" > /etc/sysconfig/network-scripts/ifcfg-eth0 && service network restart
+sudo echo "DEVICE=eth0" > /etc/sysconfig/network-scripts/ifcfg-eth0 && sudo service network restart
 
 consul agent -config-file /vagrant/scripts/ConsulServerConfig.hcl > /dev/null 2>&1 & 
 nomad agent -config /vagrant/scripts/NomadServerConfig.hcl > /dev/null 2>&1 & 
