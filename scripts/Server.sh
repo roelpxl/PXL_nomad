244063b724d5041420d59090094eee093cf6a0a7
roelpxl <56788051+roelpxl@users.noreply.github.com>
2020-10-25 02:25:15 +0200

    Update Server.sh

--- scripts/Server.sh
+++ scripts/Server.sh
@@ -5,7 +5,8 @@ sudo systemctl start nomad
 sudo echo "192.168.1.5 node" | sudo tee -a /etc/hosts
 sudo echo "192.168.1.6 node2" | sudo tee -a /etc/hosts
 
-consul agent -config-file /vagrant/scripts/ConsulServerConfig.hcl
-nomad agent -config /vagrant/scripts/NomadServerConfig.hcl &
+consul agent -config-file /vagrant/scripts/ConsulServerConfig.hcl > /dev/null 2>&1 & 
+
+nomad agent -config /vagrant/scripts/NomadServerConfig.hcl > /dev/null 2>&1 & 
 
 
