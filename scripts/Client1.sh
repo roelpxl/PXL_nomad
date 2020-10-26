bf3141010499c9c2c381cefbd77b0d9ae9b60f4e
roelpxl <56788051+roelpxl@users.noreply.github.com>
2020-10-26 20:37:57 +0100

    Update Client1.sh

--- scripts/Client1.sh
+++ scripts/Client1.sh
@@ -2,8 +2,8 @@ sudo echo "192.168.2.4 server" | sudo tee -a /etc/hosts
 
 sudo systemctl start docker
 
-sudo cat /vagrant/scripts/server/nomad.hcl > /etc/nomad.d/nomad.hcl
-sudo cat /vagrant/scripts/server/consul.hcl > /etc/consul.d/consul.hcl
+sudo cat /vagrant/scripts/client1/nomad.hcl > /etc/nomad.d/nomad.hcl
+sudo cat /vagrant/scripts/client1/consul.hcl > /etc/consul.d/consul.hcl
 
 sudo systemctl start consul 
 sudo systemctl start nomad 
