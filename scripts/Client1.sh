fe916397e8933e452721101437fd46666b5e26e3
roelpxl <56788051+roelpxl@users.noreply.github.com>
2020-10-26 20:37:41 +0100

    Update Client1.sh

--- scripts/Client1.sh
+++ scripts/Client1.sh
@@ -1,10 +1,12 @@
 sudo echo "192.168.2.4 server" | sudo tee -a /etc/hosts
 
 sudo systemctl start docker
-#sudo systemctl start consul 
-#sudo systemctl start nomad 
-sudo system consul start
-sudo system nomad start
 
-sudo consul agent -config-file /vagrant/scripts/ConsulClientConfig.hcl /dev/null 2>&1 & 
-sudo nomad agent -config /vagrant/scripts/NomadClientConfig.hcl /dev/null 2>&1 & 
+sudo cat /vagrant/scripts/server/nomad.hcl > /etc/nomad.d/nomad.hcl
+sudo cat /vagrant/scripts/server/consul.hcl > /etc/consul.d/consul.hcl
+
+sudo systemctl start consul 
+sudo systemctl start nomad 
+
+sudo consul agent /dev/null 2>&1 & 
+sudo nomad agent /dev/null 2>&1 & 
