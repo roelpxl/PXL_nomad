0b78cb7de520f4ce4c455649937c0312b9840e08
roelpxl <56788051+roelpxl@users.noreply.github.com>
2020-10-26 21:10:38 +0100

    Update Client1.sh

--- scripts/Client1.sh
+++ scripts/Client1.sh
@@ -8,5 +8,5 @@ sudo cat /vagrant/scripts/client1/consul.hcl > /etc/consul.d/consul.hcl
 sudo systemctl start consul 
 sudo systemctl start nomad 
 
-sudo consul agent /dev/null 2>&1 & 
+#sudo consul agent /dev/null 2>&1 & 
 sudo nomad agent -client -config  /etc/nomad.d/nomad.hcl -> /dev/null 2>&1 & 
