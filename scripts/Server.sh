d711a4fe376402da08e0e878a0b9196b955c8e0f
roelpxl <56788051+roelpxl@users.noreply.github.com>
2020-10-27 16:26:05 +0100

    Update Server.sh

--- scripts/Server.sh
+++ scripts/Server.sh
@@ -10,7 +10,7 @@ sudo systemctl start nomad
 #sudo consul agent > /dev/null 2>&1 & 
 #sudo nomad agent -server -config  /etc/nomad.d/nomad.hcl -> /dev/null 2>&1 & 
 
-sudo nomad job run /vagrant/scripts/webserver.hcl
+#sudo nomad job run /vagrant/scripts/webserver.hcl
 
 #sudo system consul start
 #sudo system nomad start
